4a0eaac7c2ab93a7eba366749f60021806a6fea4
phwt.smwt <phwt.smwt@gmail.com>
2020-11-17 13:24:52 +0700

    Generated spring server

---
+++ spring-server/src/main/java/io/swagger/model/Product.java
@@ -0,0 +1,309 @@
+package io.swagger.model;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import org.springframework.validation.annotation.Validated;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * Product
+ */
+@Validated
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2020-11-17T06:22:14.703Z")
+
+
+
+
+public class Product   {
+  @JsonProperty("id")
+  private Long id = null;
+
+  @JsonProperty("sku")
+  private String sku = null;
+
+  @JsonProperty("name")
+  private String name = null;
+
+  @JsonProperty("description")
+  private String description = null;
+
+  @JsonProperty("imageUrl")
+  private String imageUrl = null;
+
+  @JsonProperty("weight")
+  private Float weight = null;
+
+  @JsonProperty("price")
+  private Float price = null;
+
+  @JsonProperty("stock")
+  private Long stock = null;
+
+  @JsonProperty("shopId")
+  private Long shopId = null;
+
+  @JsonProperty("categoryId")
+  private Long categoryId = null;
+
+  public Product id(Long id) {
+    this.id = id;
+    return this;
+  }
+
+  /**
+   * Get id
+   * @return id
+  **/
+  @ApiModelProperty(value = "")
+
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public Product sku(String sku) {
+    this.sku = sku;
+    return this;
+  }
+
+  /**
+   * Get sku
+   * @return sku
+  **/
+  @ApiModelProperty(value = "")
+
+
+  public String getSku() {
+    return sku;
+  }
+
+  public void setSku(String sku) {
+    this.sku = sku;
+  }
+
+  public Product name(String name) {
+    this.name = name;
+    return this;
+  }
+
+  /**
+   * Get name
+   * @return name
+  **/
+  @ApiModelProperty(value = "")
+
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Product description(String description) {
+    this.description = description;
+    return this;
+  }
+
+  /**
+   * Get description
+   * @return description
+  **/
+  @ApiModelProperty(value = "")
+
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public Product imageUrl(String imageUrl) {
+    this.imageUrl = imageUrl;
+    return this;
+  }
+
+  /**
+   * Get imageUrl
+   * @return imageUrl
+  **/
+  @ApiModelProperty(value = "")
+
+
+  public String getImageUrl() {
+    return imageUrl;
+  }
+
+  public void setImageUrl(String imageUrl) {
+    this.imageUrl = imageUrl;
+  }
+
+  public Product weight(Float weight) {
+    this.weight = weight;
+    return this;
+  }
+
+  /**
+   * Get weight
+   * @return weight
+  **/
+  @ApiModelProperty(value = "")
+
+
+  public Float getWeight() {
+    return weight;
+  }
+
+  public void setWeight(Float weight) {
+    this.weight = weight;
+  }
+
+  public Product price(Float price) {
+    this.price = price;
+    return this;
+  }
+
+  /**
+   * Get price
+   * @return price
+  **/
+  @ApiModelProperty(value = "")
+
+
+  public Float getPrice() {
+    return price;
+  }
+
+  public void setPrice(Float price) {
+    this.price = price;
+  }
+
+  public Product stock(Long stock) {
+    this.stock = stock;
+    return this;
+  }
+
+  /**
+   * Get stock
+   * @return stock
+  **/
+  @ApiModelProperty(value = "")
+
+
+  public Long getStock() {
+    return stock;
+  }
+
+  public void setStock(Long stock) {
+    this.stock = stock;
+  }
+
+  public Product shopId(Long shopId) {
+    this.shopId = shopId;
+    return this;
+  }
+
+  /**
+   * Get shopId
+   * @return shopId
+  **/
+  @ApiModelProperty(value = "")
+
+
+  public Long getShopId() {
+    return shopId;
+  }
+
+  public void setShopId(Long shopId) {
+    this.shopId = shopId;
+  }
+
+  public Product categoryId(Long categoryId) {
+    this.categoryId = categoryId;
+    return this;
+  }
+
+  /**
+   * Get categoryId
+   * @return categoryId
+  **/
+  @ApiModelProperty(value = "")
+
+
+  public Long getCategoryId() {
+    return categoryId;
+  }
+
+  public void setCategoryId(Long categoryId) {
+    this.categoryId = categoryId;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Product product = (Product) o;
+    return Objects.equals(this.id, product.id) &&
+        Objects.equals(this.sku, product.sku) &&
+        Objects.equals(this.name, product.name) &&
+        Objects.equals(this.description, product.description) &&
+        Objects.equals(this.imageUrl, product.imageUrl) &&
+        Objects.equals(this.weight, product.weight) &&
+        Objects.equals(this.price, product.price) &&
+        Objects.equals(this.stock, product.stock) &&
+        Objects.equals(this.shopId, product.shopId) &&
+        Objects.equals(this.categoryId, product.categoryId);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, sku, name, description, imageUrl, weight, price, stock, shopId, categoryId);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class Product {\n");
+    
+    sb.append("    id: ").append(toIndentedString(id)).append("\n");
+    sb.append("    sku: ").append(toIndentedString(sku)).append("\n");
+    sb.append("    name: ").append(toIndentedString(name)).append("\n");
+    sb.append("    description: ").append(toIndentedString(description)).append("\n");
+    sb.append("    imageUrl: ").append(toIndentedString(imageUrl)).append("\n");
+    sb.append("    weight: ").append(toIndentedString(weight)).append("\n");
+    sb.append("    price: ").append(toIndentedString(price)).append("\n");
+    sb.append("    stock: ").append(toIndentedString(stock)).append("\n");
+    sb.append("    shopId: ").append(toIndentedString(shopId)).append("\n");
+    sb.append("    categoryId: ").append(toIndentedString(categoryId)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
