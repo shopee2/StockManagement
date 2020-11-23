afde7c781366390f489ba0e77c03330fb395c0bd
phwt.smwt <phwt.smwt@gmail.com>
2020-11-23 21:06:58 +0700

    Allow Cross Origin

--- stock-server/src/main/java/com/shopee2/stock/StockApplication.java
+++ stock-server/src/main/java/com/shopee2/stock/StockApplication.java
@@ -2,7 +2,10 @@ package com.shopee2.stock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @ComponentScan("com.shopee2")
@@ -10,6 +13,16 @@ public class StockApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StockApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
     }
 
 }
